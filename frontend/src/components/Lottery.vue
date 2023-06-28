23446afed0b609e8135408246f6617ba0eed8f3a
Andybabic <61660355+Andybabic@users.noreply.github.com>
2023-06-28 16:29:41 +0200

    added frontend animations

--- frontend/src/components/Lottery.vue
+++ frontend/src/components/Lottery.vue
@@ -1,11 +1,20 @@
 <template>
-    <div>
-      <h2>Enter Lottery</h2>
-      <input type="text" v-model="amount" placeholder="Amount of tokens to enter" />
-      <button @click="enterLottery">Enter Lottery</button>
+    <div class="lottery-container">
+      <transition name="fade">
+        <h2 v-if="showTitle">Enter Lottery</h2>
+      </transition>
   
-      <h2>Admin Only: Pick Winner</h2>
-      <button @click="pickWinner">Pick Winner</button>
+      <div class="input-group">
+        <input type="text" v-model="amount" placeholder="Amount of tokens to enter" />
+        <button @click="enterLottery" class="enter-button">Enter Lottery</button>
+      </div>
+  
+      <transition name="slide-fade">
+        <div v-if="showWinnerSection" class="winner-section">
+          <h2>Admin Only: Pick Winner</h2>
+          <button @click="pickWinner" class="pick-winner-button">Pick Winner</button>
+        </div>
+      </transition>
     </div>
   </template>
   
@@ -17,7 +26,9 @@
       return {
         web3: null,
         lotteryContract: null,
-        amount: ""
+        amount: "",
+        showTitle: true,
+        showWinnerSection: false,
       };
     },
     async mounted() {
@@ -30,6 +41,9 @@
   
         this.lotteryContract = new this.web3.eth.Contract(contractABI, contractAddress);
       }
+      setTimeout(() => {
+        this.showWinnerSection = true;
+      }, 1000);
     },
     methods: {
       async enterLottery() {
@@ -39,8 +53,59 @@
       async pickWinner() {
         const accounts = await this.web3.eth.getAccounts();
         await this.lotteryContract.methods.pickWinner().send({ from: accounts[0] });
-      }
-    }
+      },
+    },
   };
   </script>
+  
+  <style scoped>
+  .lottery-container {
+    text-align: center;
+    max-width: 500px;
+    margin: 0 auto;
+    padding: 40px;
+    background-color: #f9f9f9;
+    border-radius: 8px;
+  }
+  
+  .input-group {
+    margin-bottom: 20px;
+  }
+  
+  .enter-button, .pick-winner-button {
+    margin-left: 10px;
+    background-color: #4caf50;
+    color: white;
+    border: none;
+    padding: 10px 20px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+  
+  .pick-winner-button {
+    background-color: #f44336;
+  }
+  
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity 1s;
+  }
+  .fade-enter, .fade-leave-to /* .fade-leave-active in <2.1.8 */ {
+    opacity: 0;
+  }
+  
+  .slide-fade-enter-active {
+    transition: all 1s ease;
+  }
+  .slide-fade-leave-active {
+    transition: all 0.5s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+  }
+  .slide-fade-enter, .slide-fade-leave-to {
+    transform: translateY(10px);
+    opacity: 0;
+  }
+  </style>
   
